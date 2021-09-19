c75ac315e9fb26e35ea8561e29337bc34bd1d08f
AlexanderChad <Alexander.Chad@yandex.ru>
2021-09-19 17:47:42 +0700

    Update img_to_raw_rgb3.py
    Добавлена поддержка создания файлов "*.toSTM8_3bit" для проекта "matrix_32x16_on_icnd2038s"

--- img_to_raw_rgb3.py
+++ img_to_raw_rgb3.py
@@ -11,12 +11,20 @@ img = Image.open(img_path)
 x, y = img.size
 obj = img.load()
 
-raw_rgb3 = np.empty((32,16),dtype=('uint8'))
+raw_rgb3 = np.zeros((32,16),dtype=('uint8'))
+raw_rgb3_uint32 = np.zeros((3,16),dtype=('uint32'))
 for yi in range(y):
     for xi in range(x):
         r, g, b = obj[xi,yi]
         obj[xi,yi] = ((r>0)*255,(g>0)*255,(b>0)*255)
         raw_rgb3[xi][yi]=((r & 0b11111000) > 0) << 2 | ((g & 0b11111100) > 0) << 1 | (b > 0)
+        if (r>0):
+            raw_rgb3_uint32[0][yi]=raw_rgb3_uint32[0][yi]|(1 << xi)
+        if (g>0):
+            raw_rgb3_uint32[1][yi]=raw_rgb3_uint32[1][yi]|(1 << xi)
+        if (b>0):
+            raw_rgb3_uint32[2][yi]=raw_rgb3_uint32[2][yi]|(1 << xi)
+
 
 img.show()
 
@@ -44,5 +52,23 @@ for i in range(x):
             file.write(str(raw_rgb3[i,j]))
         else :
             file.write(','+str(raw_rgb3[i,j]))
+file.close()
+print("Сохранено в файл: ", raw_path)
+
+
+raw_path=img_path+'.toSTM8_3bit'
+print("Массив rgb3 для stm8:")
+for i in range(3):
+    print('\n')
+    for j in range(16):
+        print(str(format(raw_rgb3_uint32[i,j], 'b')))
+
+file = open(raw_path, 'w')
+for i in range(3):
+    for j in range(16):
+        if ((j==0)&(i==0)) :
+            file.write(str(raw_rgb3_uint32[i,j]))
+        else :
+            file.write(','+str(raw_rgb3_uint32[i,j]))
 file.close()
 print("Сохранено в файл: ", raw_path)
